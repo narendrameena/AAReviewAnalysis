0fb5ac364b8f0e720a55b287a1b45ea77f993dbe
Narendra Meena <narendrabhagri03@gmail.com>
2017-08-01 14:36:39 +0530

    add ada testing

---
+++ ADArun.R
@@ -0,0 +1,2 @@
+source("adaTesting.R")
+ADATest('data/ADAfile.txt', 0.05, 1000, 1, 'additive', TRUE) 
--- usingSkat.R
+++ usingSkat.R
@@ -1,2 +1,18 @@
 #narumeena
 #using SKAT on data
+
+install.packages("skat")
+
+
+source("http://www.bioconductor.org/biocLite.R")
+biocLite("rhdf5")
+
+install.packages(c("shiny","svDialogs","data.table","bit64"))
+library(devtools)
+install_github("mw55309/poRe_docs.git")
+#https://github.com/mw55309/poRe_docs.git
+
+library(rhdf5)
+pore_rt()
+
+library(svGUI)
